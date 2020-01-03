f9dad7f3c27de5f74c67131a5f3f90aefb8c18d1
truptigaonkar <trups11.trupti@gmail.com>
2020-01-03 14:27:11 +0100

    Component Add.js: Delete each exercise successfully

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ class Home extends React.Component {
     this.state = {
         exercises: [],
         errorMessage: '',
+        successMessage: '',
     };
 }
 
@@ -26,19 +27,43 @@ componentDidMount() {
         });
 }
 
+handleDelete(id) {
+  axios.delete(`http://localhost:5000/exercises/${id}`)
+        .then((response) => {
+          this.setState({
+            exercises: [...this.state.exercises].filter((exercise) => exercise._id !== id),
+            successMessage: 'Exercise deleted successfully'
+          });
+        })
+        .catch((error) => {
+          if (error.response.status === 400) {
+            this.setState({
+                errorMessage: 'Bad request'
+            });
+        } else if (error.response.status === 404) {
+          this.setState({
+            errorMessage: 'Not found'
+        });
+        }
+        });
+}
+
   render() {
     const { exercises } = this.state;
     const { errorMessage } = this.state;
+    const { successMessage } = this.state;
     return (
       <>
         <Helmet><title>Home</title></Helmet>
         <h3>List of exercises</h3>
         <p style={{ color: 'red' }}>{errorMessage}</p> 
+        <p style={{ color: 'green' }}>{successMessage}</p> 
         <table border="1">
           <thead>
             <tr>
               <th>Username</th>
               <th>Description</th>
+              <th>Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -46,6 +71,7 @@ componentDidMount() {
               <tr key={exercise._id}>
                 <td>{exercise.username}</td>
                 <td>{exercise.description}</td>
+                <td><button type="submit" onClick={() => this.handleDelete(exercise._id)}>Delete</button></td>
               </tr>
                         ))}
           </tbody>
