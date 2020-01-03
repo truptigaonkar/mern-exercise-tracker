b0dafa283a1bc29c5cf9778ed5408e232673179f
truptigaonkar <trups11.trupti@gmail.com>
2020-01-03 18:27:07 +0100

    Backend: users.js routes and user.modal.js added

--- backend/server.js
+++ backend/server.js
@@ -7,21 +7,23 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-//Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//Mongoose connection
+// Mongoose connection
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection;
+const { connection } = mongoose;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+    console.log('MongoDB database connection established successfully');
+});
 
 const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
 app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => { console.log(`Server is running on port: ${port}`); });
