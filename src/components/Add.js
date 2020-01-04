76916477beaa65db950d04311b713592a72dd7a2
truptigaonkar <trups11.trupti@gmail.com>
2020-01-04 15:24:38 +0100

    Component Add.js: Users can be in dropdown list

--- src/components/Add.js
+++ src/components/Add.js
@@ -8,8 +8,24 @@ class Add extends Component {
     this.state = {
         username: '',
         description: '',
-        errorMessage: ''
+        errorMessage: '',
+        users: [],
     };
+}
+
+componentDidMount() {
+// List of users
+        axios.get('http://localhost:5000/users')
+      .then((response) => {
+          this.setState({ users: response.data });
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          this.setState({
+              errorMessage: 'User with the supplied id does not exist'
+          });
+      }
+      });
 }
 
 handleAdd(e) {
@@ -39,13 +55,32 @@ handleAdd(e) {
       const { errorMessage } = this.state;
       const { username } = this.state;
       const { description } = this.state;
+      const { users } = this.state;
         return (
           <>
             <Helmet><title>Add</title></Helmet>
             <h3>Add Exercise</h3>
             <p style={{ color: 'red' }}>{errorMessage}</p>
             <form onSubmit={this.handleAdd.bind(this)}>
-              <input type="text" name="username" value={username} placeholder="Username" onChange={(e) => this.setState({ username: e.target.value })} />
+              {/* <input type="text" name="username" value={username} placeholder="Username" onChange={(e) => this.setState({ username: e.target.value })} /> */}
+              <select 
+                value={username}
+                onChange={(e) => this.setState({ username: e.target.value })}
+              >
+                <option value="select">Select</option>
+                {
+                users.map((user) => (
+                  <>
+                    <option 
+                      key={user.username}
+                      value={user.username}
+                    >
+                      {user.username}
+                    </option>
+                  </>
+))
+              }
+              </select>
               <textarea name="description" value={description} placeholder="Description" onChange={(e) => this.setState({ description: e.target.value })} /> 
               <button type="submit">Enter</button>
             </form>
