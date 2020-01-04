dad1eac830d7813248dbc23d8316e3ef57f46e0b
truptigaonkar <trups11.trupti@gmail.com>
2020-01-04 15:39:55 +0100

    Component Edit.js: Users can be in dropdown list while editing exercise

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -7,10 +7,12 @@ export default class Edit extends Component {
         this.state = {
             username: '',
             description: '',
+            users: [],
         };
     }
 
     componentDidMount() {
+      // Single exercise
         const { id } = this.props.match.params;
         axios.get(`http://localhost:5000/exercises/${id}`)
         .then((response) => {
@@ -19,6 +21,19 @@ export default class Edit extends Component {
                 description: response.data.description,
             });
         });
+
+        // List of users
+        axios.get('http://localhost:5000/users')
+      .then((response) => {
+          this.setState({ users: response.data });
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          this.setState({
+              errorMessage: 'User with the supplied id does not exist'
+          });
+      }
+      });
       }
 
     handleEdit(e) {
@@ -47,13 +62,33 @@ export default class Edit extends Component {
 
     render() {
         const { errorMessage } = this.state;
+        const { username } = this.state;
+        const { description } = this.state;
+        const { users } = this.state;
         return (
           <div>
             <h3>Edit</h3>
             <p style={{ color: 'red' }}>{errorMessage}</p>
             <form onSubmit={this.handleEdit.bind(this)}>
-              <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={(e) => this.setState({ username: e.target.value })} />
-              <textarea name="description" value={this.state.description} placeholder="Description" onChange={(e) => this.setState({ description: e.target.value })} /> 
+              {/* <input type="text" name="username" value={this.state.username} placeholder="Username" onChange={(e) => this.setState({ username: e.target.value })} /> */}
+              <select 
+                value={username}
+                onChange={(e) => this.setState({ username: e.target.value })}
+              >
+                {
+                users.map((user) => (
+                  <>
+                    <option 
+                      key={user.username}
+                      value={user.username}
+                    >
+                      {user.username}
+                    </option>
+                  </>
+))
+              }
+              </select>
+              <textarea name="description" value={description} placeholder="Description" onChange={(e) => this.setState({ description: e.target.value })} /> 
               <button type="submit">Update</button>
             </form>
           </div>
